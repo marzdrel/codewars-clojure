748c270f876328dbdb84cf6a96c3a08f43ac40e7
Marz Drel <marzdrel@dotpro.org>
2020-04-12 17:33:24 +0200

    Update spec for SkyScrappers

--- src/codewars_clojure/skyscrappers_4x4.clj
+++ src/codewars_clojure/skyscrappers_4x4.clj
@@ -5,4 +5,11 @@
        (repeat 4)
        (repeat 4)))
 
-(solve-puzzle '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0))
+(defn edge-map [index]
+  0)
+
+(defn debug [list]
+  (map println list))
+
+(debug (solve-puzzle '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0)))
+(solve-puzzle '(2 2 1 3 2 2 3 1 1 2 2 3 3 2 1 3))
