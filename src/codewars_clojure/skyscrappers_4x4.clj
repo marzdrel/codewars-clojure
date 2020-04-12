9aa4bd83981c4642425880b204de2deb4e236f49
Marz Drel <marzdrel@dotpro.org>
2020-04-12 20:43:54 +0200

    Implement initial pass for Skyscrappers 4x4

--- src/codewars_clojure/skyscrappers_4x4.clj
+++ src/codewars_clojure/skyscrappers_4x4.clj
@@ -1,15 +1,32 @@
 (ns codewars-clojure.skyscrappers-4x4)
 
+(defn- generate []
+  (vec (repeat 16 (range 1 5))))
+
+(defn initial-pass [data index clue]
+  (let
+   [regenerate (partial assoc data (edge-map index))
+    without (fn [expr] (remove expr (get data index)))]
+    (case clue
+      1 (regenerate '(4))
+      2 (regenerate (without #{4}))
+      3 (regenerate (without #{3 4}))
+      data)))
+
 (defn solve-puzzle [clues]
-  (->> 1
-       (repeat 4)
-       (repeat 4)))
+  (->> (vec clues)
+       (reduce-kv initial-pass (generate))))
 
 (defn edge-map [index]
-  0)
+  (condp > index
+    4 index
+    8 (+ 3 (* 4 (- index 4)))
+    12 (- 15 (- index 8))
+    16 (- 12 (* 4 (- index 12)))))
 
-(defn debug [list]
+(defn- debug [list]
   (map println list))
 
+(comment (solve-puzzle '(2 2 1 3 2 2 3 1 1 2 2 3 3 2 1 3)))
+
 (debug (solve-puzzle '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0)))
-(solve-puzzle '(2 2 1 3 2 2 3 1 1 2 2 3 3 2 1 3))
--- test/codewars_clojure/skyscrappers_4x4_test.clj
+++ test/codewars_clojure/skyscrappers_4x4_test.clj
@@ -2,38 +2,39 @@
   (:require [clojure.test :refer :all]
             [codewars-clojure.skyscrappers-4x4 :refer :all]))
 
-(comment
-  (deftest skyscrapers-example-test1
-    (let [clues '(2 2 1 3 2 2 3 1 1 2 2 3 3 2 1 3)
-          actual (solve-puzzle clues)]
-      (is (= (count actual) 4))
-      (is (= (nth actual 0) [1 3 4 2]))
-      (is (= (nth actual 1) [4 2 1 3]))
-      (is (= (nth actual 2) [3 4 2 1]))
-      (is (= (nth actual 3) [2 1 3 4]))))
-
-  (deftest skyscrapers-example-test2
-    (let [clues '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0)
-          actual (solve-puzzle clues)]
-      (is (= (count actual) 4))
-      (is (= (nth actual 0) '[2 1 4 3]))
-      (is (= (nth actual 1) '[3 4 1 2]))
-      (is (= (nth actual 2) '[4 2 3 1]))
-      (is (= (nth actual 3) '[1 3 2 4])))))
+; (comment
+;   (deftest skyscrapers-example-test1
+;     (let [clues '(2 2 1 3 2 2 3 1 1 2 2 3 3 2 1 3)
+;           actual (solve-puzzle clues)]
+;       (is (= (count actual) 4))
+;       (is (= (nth actual 0) [1 3 4 2]))
+;       (is (= (nth actual 1) [4 2 1 3]))
+;       (is (= (nth actual 2) [3 4 2 1]))
+;       (is (= (nth actual 3) [2 1 3 4]))))
+;
+;   (deftest skyscrapers-example-test2
+;     (let [clues '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0)
+;           actual (solve-puzzle clues)]
+;       (is (= (count actual) 4))
+;       (is (= (nth actual 0) '[2 1 4 3]))
+;       (is (= (nth actual 1) '[3 4 1 2]))
+;       (is (= (nth actual 2) '[4 2 3 1]))
+;       (is (= (nth actual 3) '[1 3 2 4])))))
 
 (deftest edge-map-test
   (is (= (edge-map 0) 0))
-  (is (= (edge-map 1) 0))
-  (is (= (edge-map 2) 0))
-  (is (= (edge-map 3) 0))
-  (is (= (edge-map 4) 0))
-  (is (= (edge-map 6) 0))
-  (is (= (edge-map 7) 0))
-  (is (= (edge-map 8) 0))
-  (is (= (edge-map 9) 0))
-  (is (= (edge-map 10) 0))
-  (is (= (edge-map 11) 0))
-  (is (= (edge-map 12) 0))
-  (is (= (edge-map 13) 0))
-  (is (= (edge-map 14) 0))
+  (is (= (edge-map 1) 1))
+  (is (= (edge-map 2) 2))
+  (is (= (edge-map 3) 3))
+  (is (= (edge-map 4) 3))
+  (is (= (edge-map 5) 7))
+  (is (= (edge-map 6) 11))
+  (is (= (edge-map 7) 15))
+  (is (= (edge-map 8) 15))
+  (is (= (edge-map 9) 14))
+  (is (= (edge-map 10) 13))
+  (is (= (edge-map 11) 12))
+  (is (= (edge-map 12) 12))
+  (is (= (edge-map 13) 8))
+  (is (= (edge-map 14) 4))
   (is (= (edge-map 15) 0)))
