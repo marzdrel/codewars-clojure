2acf1ab21d2648dcf59bebf2b26dcfedebd8c68f
Marz Drel <marzdrel@dotpro.org>
2020-04-12 16:53:07 +0200

    Scaffold Skyscrapper 4x4 Kata

--- src/codewars_clojure/skyscrappers_4x4.clj
+++ src/codewars_clojure/skyscrappers_4x4.clj
@@ -1,6 +1,8 @@
 (ns codewars-clojure.skyscrappers-4x4)
 
 (defn solve-puzzle [clues]
-  (identity 1))
+  (->> 1
+       (repeat 4)
+       (repeat 4)))
 
 (solve-puzzle '(0 0 1 2 0 2 0 0 0 3 0 0 0 1 0 0))
